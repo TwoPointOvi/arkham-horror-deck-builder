11660d5450ba8cb1b3b249ca2167b6d5ee956e81
Ovidio Villarreal <oovi@outlook.com>
2021-03-27 17:44:29 +0000

    Add and remove card from collection

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,4 +1,5 @@
-import { ButtonBase, Card, makeStyles } from '@material-ui/core';
+import { ButtonBase, Card, Icon, IconButton, makeStyles } from '@material-ui/core';
+import { BrightnessHigh, BrightnessLow } from '@material-ui/icons';
 import React from 'react';
 import { ARKHAMDB } from '../shared/urls';
 
@@ -8,34 +9,79 @@ type CardProps = {
 
 type CardState = {
     isLoading: boolean,
-    cardInfo: any
+    cardInfo: any,
+    cardInDeck: number
 }
 
 class CardDetails extends React.Component<CardProps, CardState> {
     state: CardState = {
         isLoading: true,
-        cardInfo: {}
+        cardInfo: {},
+        cardInDeck: 0
     };
+
+    componentDidMount() {
+        this.setState({
+            isLoading: true,
+            cardInfo: this.props.cardInfo,
+            cardInDeck: 0
+        });
+    }
+
+    addToDeck() {
+        const cardInDeck = this.state.cardInDeck + 1;
+        if (cardInDeck <= this.state.cardInfo.deck_limit) {
+            this.setState({
+                cardInDeck: cardInDeck 
+            });
+        } 
+    }
+
+    removeFromDeck() {
+        const cardInDeck = this.state.cardInDeck - 1;
+        if (cardInDeck < 0) return;
+        this.setState({
+            cardInDeck: cardInDeck 
+        });
+    }
 
     render() {
         return (
             <Card
                 style={{backgroundColor:'transparent'}}>
-                {/* <CardActionArea> */}
                 <ButtonBase
                     focusRipple
-                    key={this.props.cardInfo.name}
+                    key={this.state.cardInfo.name}
                     style={{
                         width: 250,
                         height: 400
                     }}
+                    onClick={() => this.addToDeck() }
                 >
                     <img
-                        style={{height:"90%", maxWidth: 300, alignSelf: 'start'}}
-                        src={ARKHAMDB + this.props.cardInfo.imagesrc}
+                        style={{height:"100%", maxWidth: 300, alignSelf: 'start'}}
+                        src={ARKHAMDB + this.state.cardInfo.imagesrc}
                     ></img>
-
                 </ButtonBase>
+                <div style={{justifyContent: 'center'}}>
+                    {
+                        Array(this.state.cardInfo.deck_limit).fill(undefined).map((_, i) => {
+                            if (i < this.state.cardInDeck) {
+                                return (
+                                    <IconButton key={this.state.cardInfo.name + i} onClick={() => this.removeFromDeck()}>
+                                        <BrightnessHigh></BrightnessHigh>
+                                    </IconButton>
+                                )
+                            } else {
+                                return (
+                                    <IconButton key={this.state.cardInfo.name + i} onClick={() => this.removeFromDeck()}>
+                                        <BrightnessLow key={this.state.cardInfo.name + i}></BrightnessLow>
+                                    </IconButton>
+                                )
+                            }
+                        })
+                    }
+                </div>
             </Card>
         );
     }
